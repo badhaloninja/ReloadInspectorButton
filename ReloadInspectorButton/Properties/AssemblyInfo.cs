f5cabe619e16a8ac6c9bd026e321a3db333a1286
badhaloninja <badhaloninja@badhalo.ninja>
2022-04-27 19:03:53 -0500

    Add project files.

---
+++ ReloadInspectorButton/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ReloadInspectorButton")]
+[assembly: AssemblyDescription("Adds a button that reloads an inspector component view")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("badhaloninja")]
+[assembly: AssemblyProduct("ReloadInspectorButton")]
+[assembly: AssemblyCopyright("Copyright © badhaloninja 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ff78500f-be67-4cfa-9f1f-9b9417a71ba4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ReloadInspectorButton/ReloadInspectorButton.cs
@@ -0,0 +1,49 @@
+using FrooxEngine;
+using HarmonyLib;
+using NeosModLoader;
+using System;
+using FrooxEngine.UIX;
+using BaseX;
+
+namespace ReloadInspectorButton
+{
+    public class ReloadInspectorButton : NeosMod
+    {
+        public override string Name => "ReloadInspectorButton";
+        public override string Author => "badhaloninja";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/badhaloninja/ReloadInspectorButton";
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("me.badhaloninja.ReloadInspectorButton");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(SceneInspector), "OnAttach")]
+        class SceneInspector_OnAttach_Patch
+        {
+            public static void Postfix(SceneInspector __instance, SyncRef<Sync<string>> ____componentText, SyncRef<Slot> ____currentComponent, SyncRef<Slot> ____componentsContentRoot)
+            {
+                UIBuilder ui = new UIBuilder(____componentText.Target.Slot.Parent);
+
+                ui.Style.FlexibleWidth = -1f;
+                ui.Style.MinWidth = 64f;
+                Uri Reload = NeosAssets.Common.Icons.Reload;
+                color color = new color(0.8f, 1f, 1f);
+                color black = color.Black;
+                var btn = ui.Button(Reload, color, black);
+                btn.LocalPressed += (button, evnt) =>
+                {
+                    if (____currentComponent.Target == null) return;
+
+                    ____componentsContentRoot.Target.DestroyChildren();
+
+                    ____componentsContentRoot.Target.AddSlot("ComponentRoot")
+                       .AttachComponent<WorkerInspector>()
+                       .SetupContainer(____currentComponent.Target);
+                };
+                btn.Slot.ActiveSelf_Field.OverrideForUser(btn.LocalUser, true).Default.Value = false;
+            }
+        }
+    }
+}
