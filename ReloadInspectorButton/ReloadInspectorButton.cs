60faaf723cc68236980cfa5d5edd8eadd7730d56
badhaloninja <badhaloninja@badhalo.ninja>
2022-05-05 13:59:08 -0500

    Add comments

--- ReloadInspectorButton/ReloadInspectorButton.cs
+++ ReloadInspectorButton/ReloadInspectorButton.cs
@@ -25,24 +25,26 @@ namespace ReloadInspectorButton
             public static void Postfix(SceneInspector __instance, SyncRef<Sync<string>> ____componentText, SyncRef<Slot> ____currentComponent, SyncRef<Slot> ____componentsContentRoot)
             {
                 UIBuilder ui = new UIBuilder(____componentText.Target.Slot.Parent);
-
+                // Fit with the size of other buttons
                 ui.Style.FlexibleWidth = -1f;
                 ui.Style.MinWidth = 64f;
+                
+                
                 Uri Reload = NeosAssets.Common.Icons.Reload;
                 color color = new color(0.8f, 1f, 1f);
-                color black = color.Black;
-                var btn = ui.Button(Reload, color, black);
+                
+                var btn = ui.Button(Reload, color, color.Black);
                 btn.LocalPressed += (button, evnt) =>
-                {
+                { // reload the inspector
                     if (____currentComponent.Target == null) return;
 
-                    ____componentsContentRoot.Target.DestroyChildren();
+                    ____componentsContentRoot.Target.DestroyChildren(); // Clear the content
 
-                    ____componentsContentRoot.Target.AddSlot("ComponentRoot")
+                    ____componentsContentRoot.Target.AddSlot("ComponentRoot") // Generate component inspector ui
                        .AttachComponent<WorkerInspector>()
                        .SetupContainer(____currentComponent.Target);
                 };
-                btn.Slot.ActiveSelf_Field.OverrideForUser(btn.LocalUser, true).Default.Value = false;
+                btn.Slot.ActiveSelf_Field.OverrideForUser(btn.LocalUser, true).Default.Value = false; // Make the button invisible for other users
             }
         }
     }
